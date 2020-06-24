1244e8bf001f471f913f6e116cabf463532ed6b7
Alejandro <alejandro.m.gomez.92@gmail.com>
2020-06-24 11:47:21 +0200

    test

--- src/views/TestChampi.vue
+++ src/views/TestChampi.vue
@@ -1,11 +1,55 @@
 <template>
-  <div class="home">
-    <h1>Test</h1>
-  </div>
+  <div id="tracking">{{positions}}</div>
 </template>
-
 <script>
 export default {
-  name: "TestChampi"
+  components: {},
+  methods: {
+    trackPosition() {
+      if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(
+          this.successPosition,
+          this.failurePosition,
+          {
+            enableHighAccuracy: true,
+            timeout: 15000,
+            maximumAge: 0
+          }
+        );
+      } else {
+        alert(`Browser doesn't support Geolocation`);
+      }
+    },
+    successPosition: function(position) {
+      this.positions.push({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      });
+      this.centerPosition = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+    },
+    failurePosition: function(err) {
+      alert("Error Code: " + err.code + " Error Message: " + err.message);
+    }
+  },
+  computed: {},
+  data: () => ({
+    centerPosition: {
+      lat: 10.762622,
+      lng: 106.660172
+    },
+    zoom: 16,
+    positions: []
+  }),
+  mounted() {
+    this.trackPosition();
+  }
 };
 </script>
+<style scoped lang="scss">
+#tracking {
+  height: 100%;
+}
+</style>
